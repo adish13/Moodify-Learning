554adfb22ab40af4b34865cca3f2f533a2f2e2c7
E-Kritheesh <81472530+E-Kritheesh@users.noreply.github.com>
2021-07-14 17:02:01 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[theme]\n\
-primaryColor = "#E694FF"\n\
-backgroundColor = "#00172B"\n\
-secondaryBackgroundColor = "#0083B8"\n\
-textColor = "#DCDCDC"\n\
-font = "sans-serif"\n\
-
 [server]\n\
 headless = true\n\
 enableCORS=false\n\
