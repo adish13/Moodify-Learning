8111c69fcbf6a9acbdd907fbf2cda82b85534478
E-Kritheesh <81472530+E-Kritheesh@users.noreply.github.com>
2021-07-15 12:32:46 +0530

    Update moodify_app.py

--- moodify_app.py
+++ moodify_app.py
@@ -20,13 +20,73 @@ st.image(os.path.join(TEAM_DIR,'Logo.png'), use_column_width = True)
 st.title('Welcome to Moodify!')
 st.write(" ------ ")
 
+st.write('''
+        # Mood based Music Recommender system
+
+     This project helps the user to automatically play songs based on the emotions of the user. 
+     It recognizes the facial emotions of the user and predicts the songs according to their mood.
+
+     **Our goal is to build a song playlist for the individual in each picture.**
+
+     👈 Please select **Select a Demo Image** to start.
+
+     📸 Feel free to upload any image you want to get a song prediction under **Upload an Image**
+
+     📞 Our team members are here to answer questions. Please refer to **Contact Information** under **Meet the Team**.''')
+st.write(" ------ ")
+
+st.sidebar.warning('\
+    Please upload SINGLE-person images. For best results, please also CENTER the person in the image.')
+st.sidebar.write(" ------ ")
+
+st.sidebar.subheader("We are the Moodify team")
+
+st.sidebar.write('''**Mentors**''')
+st.sidebar.write('Abhishek')
+st.sidebar.write('Divyanshi')
+st.sidebar.write('''**App dev**''')       
+st.sidebar.write('Aaditi')
+st.sidebar.write('Adish')
+st.sidebar.write('Bhavesh')
+st.sidebar.write('Kritheesh')
+st.sidebar.write('''**Image Classification**''')        
+st.sidebar.write('Karrthik')
+st.sidebar.write('Sai Teja')
+st.sidebar.write('Siddhant')
+st.sidebar.write('''**Music API**''')        
+st.sidebar.write('Dhawal')
+st.sidebar.write('Tanirika')
+st.sidebar.write('''**Music Classification**''')        
+st.sidebar.write('Hastyn')
+st.sidebar.write('Krutheeka')
+st.sidebar.write('Sarthak')
+
+st.sidebar.write(" ------ ")
+st.sidebar.write('Please feel free to connect with us!')
+
+expander_github = st.sidebar.beta_expander('Contact Information')
+expander_github.write('Aaditi: https://github.com/aaditist')
+expander_github.write('Adish: https://github.com/adish13')
+expander_github.write('Bhavesh: https://github.com/bhaveshkhichi')
+expander_github.write('Dhawal: https://github.com/Dhawal-AI')
+expander_github.write('Hastyn: https://github.com/Hastyn')
+expander_github.write('Karrthik: https://github.com/Karrthik-Arya')
+expander_github.write('Kritheesh: https://github.com/E-Kritheesh')
+expander_github.write('Krutheeka: https://github.com/Krutheeka-RKJ')
+expander_github.write('Sai Teja: https://github.com/tejavaranasi2')
+expander_github.write('Sarthak: https://github.com/SarthakM320')
+expander_github.write('Siddhant: https://github.com/siddhant-dutta')
+expander_github.write('Tanirika: https://github.com/TanirikaRoy')
+
+st.sidebar.success('Hope you had a great time :)')
+
 # Constants for sidebar dropdown
 SIDEBAR_OPTION_PROJECT_INFO = "Show Project Info"
 SIDEBAR_OPTION_DEMO_IMAGE = "Select a Demo Image"
 SIDEBAR_OPTION_UPLOAD_IMAGE = "Upload an Image"
 SIDEBAR_OPTION_MEET_TEAM = "Meet the Team"
 
-SIDEBAR_OPTIONS = [SIDEBAR_OPTION_PROJECT_INFO, SIDEBAR_OPTION_DEMO_IMAGE, SIDEBAR_OPTION_UPLOAD_IMAGE, SIDEBAR_OPTION_MEET_TEAM]
+SIDEBAR_OPTIONS = [SIDEBAR_OPTION_DEMO_IMAGE, SIDEBAR_OPTION_UPLOAD_IMAGE]
 
 def load_model():
     df = pd.DataFrame({
@@ -89,32 +149,32 @@ def run_app(img):
 
 def main():
 
-    st.sidebar.warning('\
-        Please upload SINGLE-person images. For best results, please also CENTER the person in the image.')
-    st.sidebar.write(" ------ ")
-    st.sidebar.title("Explore the Following")
+    #st.sidebar.warning('\
+    #    Please upload SINGLE-person images. For best results, please also CENTER the person in the image.')
+    #st.sidebar.write(" ------ ")
+    #st.sidebar.title("Explore the Following")
 
-    app_mode = st.sidebar.selectbox("Please select from the following", SIDEBAR_OPTIONS)
+    app_mode = st.radio("Please select from the following", SIDEBAR_OPTIONS)
 
-    if app_mode == SIDEBAR_OPTION_PROJECT_INFO:
-        st.sidebar.write(" ------ ")
-        st.sidebar.success("Project information showing on the right!")
-        st.write('''
+    #if app_mode == SIDEBAR_OPTION_PROJECT_INFO:
+        #st.write(" ------ ")
+        #st.sidebar.success("Project information showing on the right!")
+        #st.write('''
             # Mood based Music Recommender system
 
-         This project helps the user to automatically play songs based on the emotions of the user. 
-         It recognizes the facial emotions of the user and predicts the songs according to their mood.
+        # This project helps the user to automatically play songs based on the emotions of the user. 
+        # It recognizes the facial emotions of the user and predicts the songs according to their mood.
 
-         **Our goal is to build a song playlist for the individual in each picture.**
+        # **Our goal is to build a song playlist for the individual in each picture.**
 
-         👈 Please select **Select a Demo Image** in the sidebar to start.
+        # 👈 Please select **Select a Demo Image** to start.
 
-         📸 Feel free to upload any image you want to get a song prediction under **Upload an Image**
+        # 📸 Feel free to upload any image you want to get a song prediction under **Upload an Image**
 
-         📞 Our team members are here to answer questions. Please refer to **Contact Information** under **Meet the Team**.''')
+        # 📞 Our team members are here to answer questions. Please refer to **Contact Information** under **Meet the Team**.''')
 
-    elif app_mode == SIDEBAR_OPTION_DEMO_IMAGE:
-        st.sidebar.write(" ------ ")
+    if app_mode == SIDEBAR_OPTION_DEMO_IMAGE:
+        st.write(" ------ ")
 
         directory = os.path.join(IMAGE_DIR)
 
@@ -128,11 +188,11 @@ def main():
 
         photos.sort()
 
-        option = st.sidebar.selectbox('Please select a sample image, then click the button', photos)
-        pressed = st.sidebar.button('Create playlist')
+        option = st.selectbox('Please select a sample image, then click the button', photos)
+        pressed = st.button('Create playlist')
         if pressed:
             st.empty()
-            st.sidebar.write('Please wait for the playlist to be created! This may take up to a few minutes.')
+            st.write('Please wait for the playlist to be created! This may take up to a few minutes.')
 
             pic = os.path.join(directory, option)
 
@@ -142,63 +202,63 @@ def main():
     elif app_mode == SIDEBAR_OPTION_UPLOAD_IMAGE:
         #upload = st.empty()
         #with upload:
-        st.sidebar.info('PRIVACY POLICY: Uploaded images are never saved or stored. They are held entirely within memory for prediction \
+        st.info('PRIVACY POLICY: Uploaded images are never saved or stored. They are held entirely within memory for prediction \
             and discarded after the final results are displayed. ')
-        f = st.sidebar.file_uploader("Please Select to Upload an Image", type=['png', 'jpg', 'jpeg', 'tiff', 'gif'])
+        f = st.file_uploader("Please Select to Upload an Image", type=['png', 'jpg', 'jpeg', 'tiff', 'gif'])
         if f is not None:
             tfile = tempfile.NamedTemporaryFile(delete=True)
             tfile.write(f.read())
-            st.sidebar.write('Please wait for the playlist to be created! This may take up to a few minutes.')
+            st.write('Please wait for the playlist to be created! This may take up to a few minutes.')
             run_app(tfile)
 
 
-    elif app_mode == SIDEBAR_OPTION_MEET_TEAM:
-        st.sidebar.write(" ------ ")
-        st.subheader("We are the Moodify team")
-        first_column, second_column, third_column, fourth_column, fifth_column = st.beta_columns(5)
+    #elif app_mode == SIDEBAR_OPTION_MEET_TEAM:
+        #st.sidebar.write(" ------ ")
+        #st.subheader("We are the Moodify team")
+        #first_column, second_column, third_column, fourth_column, fifth_column = st.beta_columns(5)
 
-        first_column.write('''**Mentors**''')
-        first_column.write('Abhishek')
-        first_column.write('Divyanshi')
-        second_column.write('''**App dev**''')       
-        second_column.write('Aaditi')
-        second_column.write('Adish')
-        second_column.write('Bhavesh')
-        second_column.write('Kritheesh')
-        third_column.write('''**Image Classification**''')        
-        third_column.write('Karrthik')
-        third_column.write('Sai Teja')
-        third_column.write('Siddhant')
-        fourth_column.write('''**Music API**''')        
-        fourth_column.write('Dhawal')
-        fourth_column.write('Tanirika')
-        fifth_column.write('''**Music Classification**''')        
-        fifth_column.write('Hastyn')
-        fifth_column.write('Krutheeka')
-        fifth_column.write('Sarthak')
+        #first_column.write('''**Mentors**''')
+        #first_column.write('Abhishek')
+        #first_column.write('Divyanshi')
+        #second_column.write('''**App dev**''')       
+        #second_column.write('Aaditi')
+        #second_column.write('Adish')
+        #second_column.write('Bhavesh')
+        #second_column.write('Kritheesh')
+        #third_column.write('''**Image Classification**''')        
+        #third_column.write('Karrthik')
+        #third_column.write('Sai Teja')
+        #third_column.write('Siddhant')
+        #fourth_column.write('''**Music API**''')        
+        #fourth_column.write('Dhawal')
+        #fourth_column.write('Tanirika')
+        #fifth_column.write('''**Music Classification**''')        
+        #fifth_column.write('Hastyn')
+        #fifth_column.write('Krutheeka')
+        #fifth_column.write('Sarthak')
 
 
-        st.sidebar.write('Please feel free to connect with us!')
-        st.sidebar.success('Hope you had a great time :)')
+        #st.sidebar.write('Please feel free to connect with us!')
+        #st.sidebar.success('Hope you had a great time :)')
 
-        expandar_linkedin = st.beta_expander('Contact Information')
-        expandar_linkedin.write('Aaditi: https://github.com/aaditist')
-        expandar_linkedin.write('Adish: https://github.com/adish13')
-        expandar_linkedin.write('Bhavesh: https://github.com/bhaveshkhichi')
-        expandar_linkedin.write('Dhawal: https://github.com/Dhawal-AI')
-        expandar_linkedin.write('Hastyn: https://github.com/Hastyn')
-        expandar_linkedin.write('Karrthik: https://github.com/Karrthik-Arya')
-        expandar_linkedin.write('Kritheesh: https://github.com/E-Kritheesh')
-        expandar_linkedin.write('Krutheeka: https://github.com/Krutheeka-RKJ')
-        expandar_linkedin.write('Sai Teja: https://github.com/tejavaranasi2')
-        expandar_linkedin.write('Sarthak: https://github.com/SarthakM320')
-        expandar_linkedin.write('Siddhant: https://github.com/siddhant-dutta')
-        expandar_linkedin.write('Tanirika: https://github.com/TanirikaRoy')
+        #expander_github = st.sidebar.beta_expander('Contact Information')
+        #expander_github.write('Aaditi: https://github.com/aaditist')
+        #expander_github.write('Adish: https://github.com/adish13')
+        #expander_github.write('Bhavesh: https://github.com/bhaveshkhichi')
+        #expander_github.write('Dhawal: https://github.com/Dhawal-AI')
+        #expander_github.write('Hastyn: https://github.com/Hastyn')
+        #expander_github.write('Karrthik: https://github.com/Karrthik-Arya')
+        #expander_github.write('Kritheesh: https://github.com/E-Kritheesh')
+        #expander_github.write('Krutheeka: https://github.com/Krutheeka-RKJ')
+        #expander_github.write('Sai Teja: https://github.com/tejavaranasi2')
+        #expander_github.write('Sarthak: https://github.com/SarthakM320')
+        #expander_github.write('Siddhant: https://github.com/siddhant-dutta')
+        #expander_github.write('Tanirika: https://github.com/TanirikaRoy')
     else:
-        raise ValueError('Selected sidebar option is not implemented. Please open an issue on Github: LINK SOON')
+        raise ValueError('Selected sidebar option is not implemented. Please open an issue on Github: https://github.com/E-Kritheesh/Moodify')
 
 main()
 expander_faq = st.beta_expander("More About Our Project")
-expander_faq.write("Hi there! If you have any questions about our project, or simply want to check out the source code, please visit our github repo: LINK")
+expander_faq.write("Hi there! If you have any questions about our project, or simply want to check out the source code, please visit our github repo: https://github.com/E-Kritheesh/Moodify")
 
 
